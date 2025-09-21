5d1c4ea0fc1a2a0a1c1441f08120d91f6547687a
Justin Dela Cruz <delacruz.justin1223@gmail.com>
2025-09-21 02:18:49 -0700

    install tauri fs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -337,6 +337,7 @@ fn some_command() {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_fs::init())
     .plugin(tauri_plugin_dialog::init())
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_http::init())
